1410d7071bca8dab839d55a193258cd83cc257c5
merulis <merulis@yandex.ru>
2026-01-02 22:44:52 +0300

    add damage boxes

--- goblin.gd
+++ goblin.gd
@@ -2,7 +2,13 @@ extends CharacterBody2D
 
 @onready var goblin_sprite: Sprite2D = $GoblinSprite
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var hurtbox: Hurtbox = $HurtboxArea
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+
+func _ready() -> void:
+	hurtbox.hurt.connect(func(hitbox: Hitbox):
+		queue_free()
+	)
 
 func _physics_process(_delta: float) -> void:
 	var state = playback.get_current_node()
